de0f1f1215d9c730ae758b19f16a860b1bb122d1
AP_Dev <night00510@gmail.com>
2025-11-17 20:54:24 +0700

    Add files via upload

---
+++ PJ_Plane/sketch_nov14a_Plane/Data_Stuct.h
@@ -0,0 +1,34 @@
+#ifndef STRUCTS_H //ถ้ายังไม่ #define STRUCTS_H ให้ run ต่อ
+#define STRUCTS_H 
+
+#include <Arduino.h>
+
+// ข้อมูลจาก "เครื่องบิน" → ส่งไปรีโมท
+struct PlaneData {
+  int16_t pitch;       //ต้องเอาค่าจริง (float) ×100 แล้วเก็บลงตัวแปร int16_t
+  int16_t roll;        //ต้องเอาค่าจริง (float) ×100 แล้วเก็บลงตัวแปร int16_t
+  int16_t yaw;         // ต้องเอาค่าจริง (float) ×100 แล้วเก็บลงตัวแปร int16_t
+
+  int16_t temp;        // ต้องเอาค่าจริง (float) ×10 แล้วเก็บลงตัวแปร int16_t
+  int16_t humid;       // ต้องเอาค่าจริง (float) ×10 แล้วเก็บลงตัวแปร int16_t
+  int16_t pressure;    // ต้องเอาค่าจริง (float) ×10 แล้วเก็บลงตัวแปร int16_t
+
+  int16_t uv;          // UV GUVA เซนเซอร์ดิบ
+  int16_t battery;     // แบตเตอรี่โวลต์ 
+  int16_t height;      // ความสูงเมตร
+};
+
+// ขนาดทั้งหมด = 9 ค่า × 2 bytes = 18 bytes
+
+
+// ข้อมูลจาก "รีโมท" → ส่งไปเครื่องบิน
+struct RemoteData {
+  int16_t roll;        // จากจอยสติ๊ก    -1000 → +1000
+  int16_t pitch;       // จากจอยสติ๊ก    -1000 → +1000
+  int16_t yaw;         // จากจอยสติ๊ก    -1000 → +1000
+  int16_t thrust;      // คันเร่ง          0 → 1000
+};
+
+// ขนาด = 4 × 2 = 8 bytes
+
+#endif //ถ้าใช้ไฟล์นี้แล้ว 1 ครั้ง #ifndef STRUCTS_H จะเป็นเท็จ คอมไพล์จะ ข้ามไฟล์นี้ทั้งไฟล์ทันที
---
+++ PJ_Plane/sketch_nov14a_Plane/MPU6050.ino
@@ -0,0 +1,16 @@
+#include <Wire.h> 
+#include <MPU6050_tockn.h>
+
+MPU6050 mpu(Wire);
+
+void setup()
+{
+  Serial.begin(115200);
+  
+
+}
+
+void loop()
+{
+
+}
