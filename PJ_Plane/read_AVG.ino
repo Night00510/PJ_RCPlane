8686dd679e52e420ed90f76639364b71dd5da3fe
AP_Dev <night00510@gmail.com>
2025-11-22 15:02:37 +0700

    Update read_AVG.ino

--- PJ_Plane/read_AVG.ino
+++ PJ_Plane/read_AVG.ino
@@ -8,3 +8,15 @@ float avgRead(ReadFunc func, int n) // avgRead(ชือฟังชั้นท
 
     return sum / n;
 }
+
+float avgRead_Pin(ReadFuncPin func_Pin, int n) //avgRead_Pin(ชื่อฟังชั่นที่จะเฉลี่ล, Pinที่อ่าน, จำนวลsample);
+{
+    float sum = 0.0f;
+
+    for (int i = 0; i < n; i++){
+        sum += func_Pin();
+    }
+
+    return sum / n;
+    
+}
